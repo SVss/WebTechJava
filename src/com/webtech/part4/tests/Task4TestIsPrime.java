f6176c0f74ecddb1875fd92fb71111ad409ffc94
SVss <savionak@gmail.com>
2016-09-24 20:28:02 +0300

    Add Task 4 tests

---
+++ src/com/webtech/part4/tests/Task4TestIsPrime.java
@@ -0,0 +1,31 @@
+package com.webtech.part4.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.webtech.part4.Task4;
+
+public class Task4TestIsPrime {
+	
+	@Test
+	public void testIsPrime2() {
+		assertEquals(true, Task4.isPrime(2));
+	}
+
+	@Test
+	public void testIsPrime20() {
+		assertEquals(false, Task4.isPrime(20));
+	}
+	
+	@Test
+	public void testIsPrime21() {		
+		assertEquals(false, Task4.isPrime(21));
+	}
+	
+	@Test
+	public void testIsPrime503() {
+		assertEquals(true, Task4.isPrime(503) );
+	}
+	
+}
