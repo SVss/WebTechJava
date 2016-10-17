2ecccb2f99b1d52ca898ad1f636f994a79e2f2d0
SVss <savionak@gmail.com>
2016-10-18 02:58:46 +0300

    Complete Task 13
    * override equals(), hashCode() and toString() methods in ProgrammerBook
    class

--- lw_1/src/com/svss/webtech/lw_1/Book/ProgrammerBook.java
+++ lw_1/src/com/svss/webtech/lw_1/Book/ProgrammerBook.java
@@ -3,4 +3,30 @@ package com.svss.webtech.lw_1.Book;
 public class ProgrammerBook extends Book {
 	private String language;
 	private int level;
+	
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (!super.equals(obj)) return false;
+		if (this.getClass() != obj.getClass()) return false;
+		
+		ProgrammerBook progbook = (ProgrammerBook)obj;
+		return language.equals(progbook.language)
+				&& (level == progbook.level);
+	}
+	
+	public int hashCode() {
+		return 13*super.hashCode()
+				+ 17* language.hashCode()
+				+ 19*level
+				+ 36;
+	}
+	
+	public String toString() {
+		return "ProgrammerBook: title=\""+ title
+				+ "\" author=\"" + author
+				+ "\"" + String.format(" price=\"%d\" edition=\"%d\"", price, edition)
+				+ " language=\"" + language
+				+"\"" + String.format(" level=\"%d\"", level); 
+	}
 }
