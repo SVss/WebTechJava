47a392305cce5534ecef3c53abb654968ba59dbb
SVss <savionak@gmail.com>
2016-10-18 02:58:21 +0300

    Complete Task 12
    * override equals(), hashCode() and toString() methods in Book class

--- lw_1/src/com/svss/webtech/lw_1/Book/Book.java
+++ lw_1/src/com/svss/webtech/lw_1/Book/Book.java
@@ -5,4 +5,29 @@ public class Book {
 	protected String author;
 	protected int price;
 	protected static int edition;
+	
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (this == obj) return true;
+		if (this.getClass() != obj.getClass()) return false;
+		
+		Book book = (Book)obj;
+		return title.equals(book.title)
+				&& author.equals(book.author)
+				&& (price == book.price);
+	}
+	
+	public int hashCode() {
+		return 13*title.hashCode()
+				+ 17*author.hashCode()
+				+ 19*price
+				+ 23*edition
+				+ 78;
+	}
+	
+	public String toString() {
+		return "Book: title=\""+ title
+				+ "\" author=\"" + author
+				+ "\"" + String.format(" price=\"%d\" edition=\"%d\"", price, edition); 
+	}
 }
