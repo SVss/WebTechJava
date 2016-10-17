517bdeaee1cd61457905ac9dffaf1cef66d07ad3
SVss <savionak@gmail.com>
2016-10-18 02:25:37 +0300

    Create Book class and ProgrammerBook subclass for Tasks 12-15

---
+++ lw_1/src/com/svss/webtech/lw_1/Book/Book.java
@@ -0,0 +1,8 @@
+package com.svss.webtech.lw_1.Book;
+
+public class Book {
+	private String title;
+	private String author;
+	private int price;
+	private static int edition;
+}
---
+++ lw_1/src/com/svss/webtech/lw_1/Book/ProgrammerBook.java
@@ -0,0 +1,6 @@
+package com.svss.webtech.lw_1.Book;
+
+public class ProgrammerBook extends Book {
+	private String language;
+	private int level;
+}
