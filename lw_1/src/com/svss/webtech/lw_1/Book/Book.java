661075e0e498e1c5251ab91ffab2e33f00fe0a66
SVss <savionak@gmail.com>
2016-10-18 02:53:24 +0300

    Change Book fields access modifiers
    * for normal ProgrammerBook extension

--- lw_1/src/com/svss/webtech/lw_1/Book/Book.java
+++ lw_1/src/com/svss/webtech/lw_1/Book/Book.java
@@ -1,8 +1,8 @@
 package com.svss.webtech.lw_1.Book;
 
 public class Book {
-	private String title;
-	private String author;
-	private int price;
-	private static int edition;
+	protected String title;
+	protected String author;
+	protected int price;
+	protected static int edition;
 }
