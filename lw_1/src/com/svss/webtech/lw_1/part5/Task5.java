6fe810cc29bef8976e76d0ec2f3e5feefc2abaf9
SVss <savionak@gmail.com>
2016-10-18 17:30:19 +0300

    Complete Task 5

---
+++ lw_1/src/com/svss/webtech/lw_1/part5/Task5.java
@@ -0,0 +1,45 @@
+package com.svss.webtech.lw_1.part5;
+
+import java.util.Scanner;
+
+public class Task5 {
+	/**
+	 * @param arr - integer array to find in
+	 * @return length of maximum increasing subsequence
+	 */
+	public static int getMaxSubsequenceLength(int[] arr) {
+		int lng = arr.length;
+		int[] subseqLng = new int[lng];
+		
+		for (int i = 1; i < lng; ++i) {
+			for (int j = 0; j < i; ++j) {
+				if (arr[j] < arr[i]) {
+					subseqLng[i] = Math.max(subseqLng[i], subseqLng[j]+1);
+				}
+			}
+		}
+		
+		int result = subseqLng[0];
+		for (int i = 1; i < lng; ++i) {
+			result = Math.max(result, subseqLng[i]);
+		}
+		return result + 1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter array size: ");
+		int n = in.nextInt();
+		
+		System.out.println("Enter array elements:");
+		int[] arr = new int[n];
+		for (int i = 0; i < n; ++i) {
+			arr[i] = in.nextInt();
+		}
+		in.close();
+		
+		int result = arr.length - getMaxSubsequenceLength(arr);
+		System.out.println(String.format("Minimum elements count to remove: %d", result));
+	}
+}
